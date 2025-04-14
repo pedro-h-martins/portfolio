f3457b26e4d875049761675ad9c6e675e43bc32f
pedro-h-martins <pedrohenrimartinss@gmail.com>
2025-04-14 14:21:19 -0300

    feat: adicionado renderizacao dinamica de data para 'index.ejs'

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
+const portfolioData = {
+    name: "Pedro Henrique Martins",
+    role: "Estudante",
+    email: "pedrohenrimartinss@gmail.com",
+    interests: "Aprendizado de Máquina e Back-end de sites.",
+    description: "Sou estudante do curso de Desenvolvimento de Software Multiplataforma, no qual tenho muito interrese em desenvolvimento de software. Busco aprimorar minhas habilidades para ingressar projetos que contenham Aprendizado de Máquina e Back-end de sites."
+};
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { data: portfolioData });
 });
 
 const port = 3000;
